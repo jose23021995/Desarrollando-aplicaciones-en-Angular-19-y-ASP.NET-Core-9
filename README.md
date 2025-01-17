c98d0c289ccf4477203d642fd0be87e1c9feb837
jose23021995 <mtjamx95@gmail.com>
2025-01-17 12:48:00 -0600

    interpolacion

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Angular 19 / NetCore
 Desarrollando-aplicaciones-en-Angular-19-y-ASP.NET-Core-9  
 curso de udemy - angular y net core
+
+# Glosario
+1- interpolación - La interpolación de cadenas en Angular es el mecanismo para sustituir una expresión por un valor de tipo string en la plantilla (template).
--- peliculas1/src/app/app.component.ts
+++ peliculas1/src/app/app.component.ts
@@ -3,10 +3,19 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone:true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-peliculas';
+  title = 'Primer proyecto en angular 19';
+  edad=29;
+  pelcula={
+    titulo:"spider man",
+    fechaLanzamiento:new Date()
+  }
+  duplicarNumero(valor:number){
+    return valor *valor;
+  }
 }
