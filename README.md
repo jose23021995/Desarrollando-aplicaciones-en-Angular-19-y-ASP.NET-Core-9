218095e02099b7f4841ea01febb384538a9fb60d
jose23021995 <mtjamx95@gmail.com>
2025-01-17 13:27:42 -0600

    se genera modulo numero 2

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ curso de udemy - angular y net core
 
 # Glosario
 1- interpolación - La interpolación de cadenas en Angular es el mecanismo para sustituir una expresión por un valor de tipo string en la plantilla (template).
+2- Pipe -
+3- 
--- peliculas1/src/app/app.component.ts
+++ peliculas1/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet],
+  imports: [DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
