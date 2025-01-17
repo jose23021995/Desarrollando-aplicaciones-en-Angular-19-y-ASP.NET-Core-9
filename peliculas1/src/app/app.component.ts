1abfb646adfb2524727202b314f723fe58b32569
jose23021995 <mtjamx95@gmail.com>
2025-01-17 14:11:01 -0600

    ngFor

--- peliculas1/src/app/app.component.ts
+++ peliculas1/src/app/app.component.ts
@@ -1,22 +1,39 @@
-import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgFor, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Primer proyecto en angular 19';
   edad=29;
-  pelcula={
-    titulo:"spider man",
-    fechaLanzamiento:new Date(),
-    precio:1400.99
-  }
+  pelculas=[
+    {
+      titulo:"spider man",
+      fechaLanzamiento:new Date(),
+      precio:1400.99
+    },
+    {
+      titulo:"venom",
+      fechaLanzamiento:new Date(),
+      precio:999
+    },
+    {
+      titulo:"marvel",
+      fechaLanzamiento:new Date(),
+      precio:300.99
+    },
+    {
+      titulo:"DC",
+      fechaLanzamiento:new Date(),
+      precio:300.99
+    },
+  ];
   duplicarNumero(valor:number){
     return valor *valor;
   }
