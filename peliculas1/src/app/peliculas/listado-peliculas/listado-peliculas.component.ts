25e0547d76b4f62d6f165e72d16b07c1439417ed
jose23021995 <mtjamx95@gmail.com>
2025-01-20 20:14:30 -0600

    instalacion de angular material

--- peliculas1/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ peliculas1/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { ListadoGenericoComponent } from "../../compartidos/componentes/listado-generico/listado-generico.component";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon"
 
 @Component({
   selector: 'app-listado-peliculas',
-  imports: [DatePipe, CurrencyPipe, ListadoGenericoComponent],
+  imports: [DatePipe, CurrencyPipe, ListadoGenericoComponent,MatButtonModule,MatIconModule
+  ],
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css'
 })
