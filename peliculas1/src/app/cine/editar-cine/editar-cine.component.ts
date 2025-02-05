1a9dbaba0d7fec3c0f8e6bff52f7e30296622053
jose23021995 <mtjamx95@gmail.com>
2025-02-04 21:12:59 -0600

    se generar componente de mapas

--- peliculas1/src/app/cine/editar-cine/editar-cine.component.ts
+++ peliculas1/src/app/cine/editar-cine/editar-cine.component.ts
@@ -1,12 +1,18 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { CineCreacionDTO, CineDTO } from '../cines';
+import { FormularioCinesComponent } from "../formulario-cines/formulario-cines.component";
 
 @Component({
   selector: 'app-editar-cine',
-  imports: [],
+  imports: [FormularioCinesComponent],
   templateUrl: './editar-cine.component.html',
   styleUrl: './editar-cine.component.css'
 })
 export class EditarCineComponent {
   @Input(  {transform:numberAttribute})
     id!:number;
+    cine:CineDTO={id:1,nombre:'cinemex',latitud:19.440519,longitud:-99.094216}
+    guardarCambios(cine:CineCreacionDTO){
+      console.log("editar cine",cine)
+    }
 }
