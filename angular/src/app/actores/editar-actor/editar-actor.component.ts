2dd241c71d78c1f3d08080025681a39e64cb7b1e
jose23021995 <mtjamx95@gmail.com>
2025-04-01 15:54:32 -0600

    editar actores y generos

--- angular/src/app/actores/editar-actor/editar-actor.component.ts
+++ angular/src/app/actores/editar-actor/editar-actor.component.ts
@@ -1,38 +1,27 @@
-import { Component, Inject, inject, Input, numberAttribute, OnInit } from '@angular/core';
+import { Component, inject, Input, numberAttribute, OnInit } from '@angular/core';
 import { ActorCreacionDTO, ActorDTO } from '../actores';
 import { FormularioActoresComponent } from "../formulario-actores/formulario-actores.component";
 import { ActoresService } from '../actores.service';
+import { Router } from '@angular/router';
 import { exportarErrores } from '../../compartidos/funciones/ExtraerErrores';
 import { MostrarErroresComponent } from "../../compartidos/componentes/mostrar-errores/mostrar-errores.component";
 import { CargandoComponent } from "../../compartidos/componentes/cargando/cargando.component";
-import { Router } from '@angular/router';
+import { EditarEntidadComponent } from "../../compartidos/componentes/editar-entidad/editar-entidad.component";
+import { SERVICIO_CRUD_TOKEN } from '../../compartidos/proveedores/proveedores';
 
 @Component({
-  selector: 'app-editar-actor',
-  imports: [FormularioActoresComponent, MostrarErroresComponent, CargandoComponent],
-  templateUrl: './editar-actor.component.html',
-  styleUrl: './editar-actor.component.css'
+    selector: 'app-editar-actor',
+    imports: [EditarEntidadComponent],
+    templateUrl: './editar-actor.component.html',
+    styleUrl: './editar-actor.component.css',
+    providers: [
+        { provide: SERVICIO_CRUD_TOKEN, useClass: ActoresService }
+    ]
 })
-export class EditarActorComponent implements OnInit{
+export class EditarActorComponent {
+  @Input({transform: numberAttribute})
+  id!: number;
 
-  ngOnInit(): void {
-    this.actoresService.obtenerPorId(this.id).subscribe(actor=>{this.actor=actor})
-  }
-@Input(  {transform:numberAttribute})
-  id!:number;
-  actor?:ActorDTO;
-  actoresService=inject(ActoresService);
-  router= inject(Router);
-  errores:string[]=[];
-  guardarCambios(actor:ActorCreacionDTO){
-    this.actoresService.actualizar(this.id,actor).subscribe({
-      next:()=>{
-        this.router.navigate(['/actores']);
-      },
-      error:err=>{
-        const errores=exportarErrores(err);
-        this.errores=errores;
-      }
-    });
-  }
+  formularioActores = FormularioActoresComponent;
+
 }
--- angular/src/app/genero/editar-genero/editar-genero.component.ts
+++ angular/src/app/genero/editar-genero/editar-genero.component.ts
@@ -1,41 +1,21 @@
 import { Component, inject, Input, numberAttribute, OnInit } from '@angular/core';
 import { FormularioGeneroComponent } from "../formulario-genero/formulario-genero.component";
-import { GeneroCreacionDTO, GeneroDTO } from '../generos';
 import { GenerosService } from '../generos.service';
-import { MostrarErroresComponent } from "../../compartidos/componentes/mostrar-errores/mostrar-errores.component";
-import { Router } from '@angular/router';
-import { exportarErrores } from '../../compartidos/funciones/ExtraerErrores';
-import { CargandoComponent } from "../../compartidos/componentes/cargando/cargando.component";
+import { SERVICIO_CRUD_TOKEN } from '../../compartidos/proveedores/proveedores';
+import { EditarEntidadComponent } from "../../compartidos/componentes/editar-entidad/editar-entidad.component";
 
 @Component({
-  selector: 'app-editar-genero',
-  imports: [FormularioGeneroComponent, MostrarErroresComponent, CargandoComponent],
-  templateUrl: './editar-genero.component.html',
-  styleUrl: './editar-genero.component.css'
+    selector: 'app-editar-genero',
+    imports: [EditarEntidadComponent],
+    templateUrl: './editar-genero.component.html',
+    styleUrl: './editar-genero.component.css',
+    providers: [
+        { provide: SERVICIO_CRUD_TOKEN, useClass: GenerosService }
+    ]
 })
-export class EditarGeneroComponent implements OnInit{
-  ngOnInit(): void {
-    this.generosService.obtenerPorId(this.id).subscribe(genero=>{
-      this.genero=genero;
-    })
-  }
-  @Input(  {transform:numberAttribute})
-  id!:number; 
-  genero?:GeneroDTO ;
-  generosService=inject(GenerosService);
-  errores:string[]=[];
-  router=inject(Router);
-  guardarCambios(genero:GeneroCreacionDTO){
-      //console.log("Editando el genero ->",genero);
-      this.generosService.actualizar(this.id,genero).subscribe({
-        next:()=>{
-          this.router.navigate(['/generos']);
-        },
-        error:err=>{
-          const errores= exportarErrores(err);
-          this.errores=errores;
-        }
-      });
-  }
-  
+export class EditarGeneroComponent {
+
+  @Input({ transform: numberAttribute })
+  id!: number;
+  formularioGenero = FormularioGeneroComponent;
 }
